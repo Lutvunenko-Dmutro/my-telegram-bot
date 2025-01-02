eb4c4a3d59f9d350600be0a4e8f6e8918538d151
Литвиненко Дмитро <154090291+Lutvunenko-Dmutro@users.noreply.github.com>
2025-01-02 13:00:41 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -15,7 +15,7 @@ load_dotenv()
 
 # Встановлення логування
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelень)s - %(message)s',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',  # Виправлення ключа на 'levelname'
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
